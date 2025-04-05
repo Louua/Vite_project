6ab8aac5be78b88d57ddfbdfc8de4bd98df9381a
Loua Ben Afia <loua12benafia@gmail.com>
2025-04-05 17:27:50 +0100

    SET UP

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+const Card = ( {title}) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      
+    </div>
+  )
+}
+const App = () => {
+  return (
+    <div className='card-container'>
+      < Card title="Star Wars" />
+      < Card  title="Avatar"/>
+      < Card title=" The Lion King"/>
+
+
+    </div>
+  )
+}
+
+export default App
